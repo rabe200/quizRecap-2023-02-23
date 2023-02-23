380d889e0b8466d7be951c85cdf2e4c4d10374e3
Rabe Christian <rabenklar@gmx.de>
2023-02-23 10:57:08 +0100

    add answer toggle

--- index.js
+++ index.js
@@ -3,7 +3,18 @@
 const bookmark = document.querySelector('.bookmark');
 console.log("bookmark: " + bookmark.innerHTML);
 
+
 bookmark.addEventListener("click", () => {
     bookmark.classList.toggle("bookmark--active");
-    console.log("<-- bookmark clicked!!! --...>");
+    console.log("<-- bookmark clicked!!!! --...>");
+});
+
+const answer = document.querySelector('.card__button-answer');
+console.log("card: " + answer.innerHTML);
+const card__answer=document.querySelector('.card__answer');
+
+answer.addEventListener("click", () => {
+    
+    console.log("<--- show answer clicked --->");
+    card__answer.classList.toggle("card__answer--active");
 });
