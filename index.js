4e859df6083845af48d4a112d6c98b7729967ba1
Rabe Christian <rabenklar@gmx.de>
2023-02-23 10:42:35 +0100

    change some exclamation

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // phew… not a lot going on here. Please add some code!
 
-const bookmark = document.querySelectorAll('.bookmark');
+const bookmark = document.querySelector('.bookmark');
 console.log("bookmark: " + bookmark.innerHTML);
+
+bookmark.addEventListener("click", () => {
+    bookmark.classList.toggle("bookmark--active");
+    console.log("<-- bookmark clicked!!! --...>");
+});
