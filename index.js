a9076c80764b9eacaccdb14ed985d681d14c350e
Rabe Christian <rabenklar@gmx.de>
2023-02-23 17:34:13 +0100

    fix index.js

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-const bookmark = document.querySelectorAll('.bookmark');
+
+// phew… not a lot going on here. Please add some code!
+
+const bookmark = document.querySelector('.bookmark');
 console.log("bookmark: " + bookmark.innerHTML);
+
+
+bookmark.addEventListener("click", () => {
+    bookmark.classList.toggle("bookmark--active");
+    console.log("<-- bookmark clicked!!!! --...>");
+});
+
+const answer = document.querySelector('.card__button-answer');
+console.log("card: " + answer.innerHTML);
+const card__answer=document.querySelector('.card__answer');
+
+answer.addEventListener("click", () => {
+    
+    console.log("<--- show answer clicked --->");
+    
+    if (card__answer.classList.contains("card__answer--active"))
+ {   console.log("hidden answer");    
+    answer.textContent = "Show answer";
+}
+    else {
+        console.log("Displayed answeer");
+answer.textContent ="Hide answer" ;
+console.log(answer)}
+
+    
+
+    card__answer.classList.toggle("card__answer--active");
+});
